ae7c10250e9fe240c58e098e2e4c9fa87ba36f19
atokoamuk <kt.kmo814@outlook.com>
2025-03-31 17:17:17 +0900

    feat: ESLintとPrettierの設定を追加
    - .prettierrcを新規作成し、Prettierの設定を追加
    - eslint.config.mjsにPrettierの推奨設定を統合
    - package.jsonに必要な依存関係を追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslintConfigPrettier, { rules } from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,46 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "no-unused-vars": "warn",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": ["error"],
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+  },
+  {
+    files: ["**/page.tsx", "**/layout.tsx", "**/template.tsx"],
+    rules: {
+      "import/no-default-export": "off",
+      "import/prefer-default-export": "off",
+    },
+  },
+  eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      import: eslintPluginImport,
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
