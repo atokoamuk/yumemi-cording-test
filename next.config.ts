745925b7d4270d4c35f499de63c6fd3c9e7a2c1b
atokoamuk <kt.kmo814@outlook.com>
2025-03-31 17:17:29 +0900

    feat: Next.jsの設定を更新
    - 開発環境に応じてdistDirを設定
    - 出力形式をexportに変更

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isDev = process.env.NODE_ENV === "development";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  distDir: isDev ? undefined : "dist",
 };
 
 export default nextConfig;
