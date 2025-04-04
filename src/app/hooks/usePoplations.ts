3342b5ef6d4a209ff20df97a5022c61477686acd
atokoamuk <kt.kmo814@outlook.com>
2025-04-04 15:14:54 +0900

    feat: 都道府県の人口データと都道府県情報を取得するフックを追加
    - usePopulationフックを追加し、指定した都道府県の人口データを取得する機能を実装
    - usePrefecturesフックを追加し、都道府県のリストを取得する機能を実装

---
+++ src/app/hooks/usePoplations.ts
@@ -0,0 +1,37 @@
+// usePopulationData.ts
+import { useQueries } from '@tanstack/react-query'
+import { useMemo } from 'react'
+import { getPopulation } from '../actions'
+import { PopulationLabel } from '../type'
+
+export function usePopulation(prefcodes: number[], label: PopulationLabel) {
+  const populationQueries = useQueries({
+    queries: prefcodes.map((prefCode) => ({
+      queryKey: ['population', prefCode],
+      queryFn: () => getPopulation(prefCode),
+    })),
+  })
+
+  const populations = useMemo(() => {
+    const datas = populationQueries.map(({ data }) => data).filter((data) => !!data)
+    const data: { year: number; [key: string]: number }[] = []
+    datas.forEach(({ result, prefCode }) => {
+      const target = result.data.find((item) => item.label === label)
+      if (target) {
+        target.data.forEach(({ year, value }) => {
+          const existIndex = data.findIndex((d) => d.year === year)
+          if (existIndex !== -1) {
+            data[existIndex][prefCode] = value
+          } else {
+            data.push({ year, [prefCode]: value })
+          }
+        })
+      }
+    })
+    return data
+  }, [populationQueries, label])
+
+  const isLoading = populationQueries.some((query) => query.isLoading)
+
+  return { isLoading, populations }
+}
