63535d78a96186cc80ed8cebc85db06bda7d4e63
atokoamuk <kt.kmo814@outlook.com>
2025-04-04 15:30:18 +0900

    feat: コンポーネントとカスタムフックの分割を行い実装
    - 不要なクエリを削除し、カスタムフックを使用して都道府県と人口データを取得
    - 都道府県のチェックボックスコンポーネントを作成し、選択状態を管理
    - ラベルタブコンポーネントを追加し、選択したラベルに基づいて人口データを表示
    - スケルトンコンポーネントを使用して、データ読み込み中のUIを改善

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,93 +1,61 @@
 'use client'
 
-import { useEffect, useMemo, useState } from 'react'
-import { getPopulation, getPrefectures } from './actions'
-import { useQueries, useQuery } from '@tanstack/react-query'
-import { LineChart, Line, XAxis, YAxis, ResponsiveContainer, Legend } from 'recharts'
+import { useMemo, useState } from 'react'
+import PopulationGraph from './components/PoplationGraph'
+import { PopulationLabel } from './type'
+import LabelTab from './components/LabelTab'
+import PrefectureCheckbox from './components/PrefecturesCheckbox'
+import Skeleton from '@/components/Skelton'
+import { usePrefectures } from './hooks/usePrefectures'
+import { usePopulation } from './hooks/usePoplations'
 
 export default function Home() {
-  const prefectureQuery = useQuery({
-    queryKey: ['prefectures'],
-    queryFn: getPrefectures,
-  })
-
-  const prefectures = prefectureQuery.data || []
+  const { prefectures } = usePrefectures()
 
   const [selectedPrefcode, setSelectedPrefcode] = useState<number[]>([])
 
   const selectedPrefectures = useMemo(
     () => prefectures.filter((prefecture) => selectedPrefcode.includes(prefecture.prefCode)),
-    [selectedPrefcode],
+    [prefectures, selectedPrefcode],
   )
+  const [selectedLabel, setSelectedLabel] = useState<PopulationLabel>('総人口')
 
-  function handleCheckboxChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const value = parseInt(event.target.value, 10)
-    if (event.target.checked) {
-      setSelectedPrefcode((prev) => [...prev, value])
-    } else {
-      setSelectedPrefcode((prev) => prev.filter((prefCode) => prefCode !== value))
-    }
+  const { populations } = usePopulation(selectedPrefcode, selectedLabel)
+
+  function handleSelectedPrefcodeChange(prefcode: number) {
+    setSelectedPrefcode((prev) =>
+      prev.includes(prefcode) ? prev.filter((code) => code !== prefcode) : [...prev, prefcode],
+    )
   }
 
-  const populationQueries = useQueries({
-    queries: selectedPrefcode.map((prefCode) => ({
-      queryKey: ['population', prefCode],
-      queryFn: () => getPopulation(prefCode),
-    })),
-  })
-
-  const populationData = populationQueries.map(({ data }) => data).filter((data) => !!data)
-
-  const graphData = useMemo(() => {
-    const data: { year: number; [key: string]: number }[] = []
-    populationData.forEach(({ result, prefCode }) => {
-      const target = result.data.find((item) => item.label === '総人口')
-      if (target) {
-        target.data.forEach(({ year, value }) => {
-          const existIndex = data.findIndex((d) => d.year === year)
-
-          if (existIndex !== -1) {
-            data[existIndex][prefCode] = value
-          } else {
-            data.push({ year, [prefCode]: value })
-          }
-        })
-      }
-    })
-    return data
-  }, [populationData])
-
-  useEffect(() => {
-    console.log('graphData', graphData)
-  }, [graphData])
-
   return (
-    <div className="flex flex-col min-h-screen p-24">
+    <div className="flex flex-col h-svh p-24 gap-8">
       <h1 className="text-3xl font-bold">都道府県</h1>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-6">
-        {prefectures.map((prefecture) => (
-          <label key={prefecture.prefCode} className="flex items-center space-x-2">
-            <input type="checkbox" value={prefecture.prefCode} onChange={handleCheckboxChange} />
-            <span>{prefecture.prefName}</span>
-          </label>
-        ))}
+      <div className="h-[30%] overflow-y-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+        {prefectures.length > 0 ? (
+          <PrefectureCheckbox
+            prefectures={prefectures}
+            selectedPrefcodes={selectedPrefcode}
+            onChange={handleSelectedPrefcodeChange}
+          />
+        ) : (
+          [...Array(47)].map((_, index) => (
+            <div key={`prefecture-skeleton-${index}`} className="h-[1em]">
+              <Skeleton key={index} />
+            </div>
+          ))
+        )}
       </div>
-      <ResponsiveContainer width="100%" height="40%">
-        <LineChart data={graphData}>
-          <XAxis dataKey="year" />
-          <YAxis />
-          <Legend />
-          {selectedPrefectures.map((prefecture) => (
-            <Line
-              key={`line-${prefecture.prefCode}`}
-              name={`${prefecture.prefName}`}
-              type="monotone"
-              dataKey={`${prefecture.prefCode}`}
-              stroke={`hsl(${(prefecture.prefCode * 137.5) % 360}, 70%, 50%)`}
-            />
-          ))}
-        </LineChart>
-      </ResponsiveContainer>
+      <LabelTab selectedLabel={selectedLabel} onChange={(s) => setSelectedLabel(s)} />
+      <div className="w-full bg-white flex-1 p-8">
+        {selectedPrefectures.length > 0 ? (
+          <PopulationGraph data={populations} prefectures={selectedPrefectures} />
+        ) : (
+          <div className="flex justify-center items-center h-full">
+            <h1 className="text-gray-500 text-2xl">都道府県を選択してください</h1>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
