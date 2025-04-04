69fd9f04b205f948e301f2e7abf02df961e327a2
atokoamuk <kt.kmo814@outlook.com>
2025-04-04 23:54:40 +0900

    feat: ESLint設定を更新し、Prettierとの互換性を追加
    - ESLintの設定にPrettierを追加
    - 不要なルールをオフに設定
    - インポートプラグインを追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,58 +1,49 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import eslintConfigPrettier, { rules } from "eslint-config-prettier";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintPluginImport from 'eslint-plugin-import'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
-    files: ["**/*.ts", "**/*.tsx"],
+    files: ['**/*.ts', '**/*.tsx'],
     rules: {
-      "no-unused-vars": "warn",
-      "react/react-in-jsx-scope": "off",
-      "@typescript-eslint/no-unused-vars": ["error"],
-      "@typescript-eslint/explicit-module-boundary-types": "off",
+      'no-unused-vars': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
     },
   },
   {
-    files: ["**/page.tsx", "**/layout.tsx", "**/template.tsx"],
+    files: ['**/page.tsx', '**/layout.tsx', '**/template.tsx'],
     rules: {
-      "import/no-default-export": "off",
-      "import/prefer-default-export": "off",
+      'import/no-default-export': 'off',
+      'import/prefer-default-export': 'off',
     },
   },
-  eslintPluginPrettierRecommended,
-  eslintConfigPrettier,
   {
     plugins: {
       import: eslintPluginImport,
     },
     rules: {
-      "import/order": [
-        "error",
+      'import/order': [
+        'error',
         {
-          groups: [
-            "builtin",
-            "external",
-            "internal",
-            "parent",
-            "sibling",
-            "index",
-          ],
-          "newlines-between": "always",
-          alphabetize: { order: "asc", caseInsensitive: true },
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
         },
       ],
     },
   },
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
