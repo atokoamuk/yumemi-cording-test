d0a71a70094cc089b8650bb13a929e581cd7b058
atokoamuk <kt.kmo814@outlook.com>
2025-04-08 16:15:33 +0900

    refactor: Rootコンポーネントのリファクタリングと機能改善
    - useCallbackを使用してhandleChangeSelectedPrefcodeとhandleLabelChangeを最適化
    - 不要なコードを削除し、可読性を向上
    - コンポーネント名をHomeからRootに変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useMemo, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 
 import LoadingOverlay from '@/components/LoadingOverlay'
 import Skeleton from '@/components/Skelton'
@@ -12,46 +12,59 @@ import { usePopulation } from './hooks/usePoplations'
 import { usePrefectures } from './hooks/usePrefectures'
 import { PopulationLabel } from './type'
 
-
-export default function Home() {
-  const { prefectures } = usePrefectures()
-
+export default function Root() {
   const [selectedPrefcode, setSelectedPrefcode] = useState<number[]>([])
+  const [selectedLabel, setSelectedLabel] = useState<PopulationLabel>('総人口')
+
+  const { prefectures } = usePrefectures()
+  const { populations, isLoading } = usePopulation(selectedPrefcode, selectedLabel)
+
+  const handleChangeSelectedPrefcode = useCallback(
+    (prefcode: number) => {
+      setSelectedPrefcode((prev) =>
+        prev.includes(prefcode) ? prev.filter((code) => code !== prefcode) : [...prev, prefcode],
+      )
+    },
+    [setSelectedPrefcode],
+  )
+
+  const handleLabelChange = useCallback(
+    (label: PopulationLabel) => {
+      setSelectedLabel(label)
+    },
+    [setSelectedLabel],
+  )
 
   const selectedPrefectures = useMemo(
     () => prefectures.filter((prefecture) => selectedPrefcode.includes(prefecture.prefCode)),
     [prefectures, selectedPrefcode],
   )
-  const [selectedLabel, setSelectedLabel] = useState<PopulationLabel>('総人口')
-
-  const { populations, isLoading } = usePopulation(selectedPrefcode, selectedLabel)
-
-  function handleSelectedPrefcodeChange(prefcode: number) {
-    setSelectedPrefcode((prev) =>
-      prev.includes(prefcode) ? prev.filter((code) => code !== prefcode) : [...prev, prefcode],
-    )
-  }
 
   return (
-    <div className="flex flex-col h-svh p-24 gap-8">
-      <h1 className="text-3xl font-bold">都道府県</h1>
+    <div className="flex flex-col h-svh px-16 py-8 gap-4">
+      <div className="flex items-center gap-4">
+        <h1 className="font-bold">都道府県</h1>
+      </div>
+
       <div className="h-[30%] overflow-y-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
         {prefectures.length > 0 ? (
           <PrefectureCheckbox
             prefectures={prefectures}
             selectedPrefcodes={selectedPrefcode}
-            onChange={handleSelectedPrefcodeChange}
+            onChange={handleChangeSelectedPrefcode}
           />
         ) : (
           [...Array(47)].map((_, index) => (
-            <div key={`prefecture-skeleton-${index}`} className="h-[1em]">
+            <div data-testid={`prefecture-skeleton-${index}`} key={`prefecture-skeleton-${index}`} className="h-[1em]">
               <Skeleton key={index} />
             </div>
           ))
         )}
       </div>
-      <LabelTab selectedLabel={selectedLabel} onChange={(s) => setSelectedLabel(s)} />
-      <div className="w-full bg-white flex-1 p-8 relative">
+
+      <LabelTab selectedLabel={selectedLabel} onChange={handleLabelChange} />
+
+      <div className="w-full bg-white flex-1 relative">
         {selectedPrefectures.length > 0 ? (
           <>
             <LoadingOverlay show={isLoading} />
