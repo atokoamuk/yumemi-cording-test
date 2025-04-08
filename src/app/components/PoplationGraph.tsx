414ceeb3438260a001b641416ecd300c592cd800
atokoamuk <kt.kmo814@outlook.com>
2025-04-08 16:13:19 +0900

    feat: #3 PopulationGraphコンポーネントのスタイルと機能を改善
    - memoでラップし、再レンダリングを最適化
    - レジェンドのマウスイベントハンドラを改善
    - グラフのスタイルを調整し、ラベルを追加
    - レスポンシブコンテナのマージンを設定

--- src/app/components/PoplationGraph.tsx
+++ src/app/components/PoplationGraph.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { LineChart, Line, XAxis, YAxis, ResponsiveContainer, Legend, CartesianGrid, Tooltip } from 'recharts'
+import { memo, useState } from 'react'
+import { LineChart, Line, XAxis, YAxis, ResponsiveContainer, Legend, CartesianGrid, Tooltip, Label } from 'recharts'
 import { Payload } from 'recharts/types/component/DefaultLegendContent'
 
 import { Prefecture } from '../type'
@@ -9,7 +9,7 @@ type Props = {
   prefectures: Prefecture[]
 }
 
-export default function PopulationGraph(props: Props) {
+function PopulationGraph(props: Props) {
   const { data, prefectures } = props
 
   const [targetDatakey, setTargetDatakey] = useState<string | undefined>()
@@ -25,31 +25,40 @@ export default function PopulationGraph(props: Props) {
   }
 
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <LineChart data={data}>
-        <XAxis dataKey="year" />
-        <YAxis />
-        <Legend
-          onMouseEnter={handleLegendMouseEnter}
-          onMouseOut={handleLegendMouseOut}
-          align="right"
-          layout="vertical"
-          verticalAlign="top"
-          wrapperStyle={{ paddingLeft: '2rem', maxHeight: '100%', overflowY: 'auto' }}
-        />
-        <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-        <Tooltip />
-        {prefectures.map((prefecture) => (
-          <Line
-            strokeWidth={targetDatakey === `${prefecture.prefCode}` ? 4 : 1}
-            key={`line-${prefecture.prefCode}`}
-            name={`${prefecture.prefName}`}
-            type="monotone"
-            dataKey={`${prefecture.prefCode}`}
-            stroke={`hsl(${(prefecture.prefCode * 360) / 47}, 75%, 60%)`}
+    <div className="w-full flex flex-col h-full items-center gap-2 p-2">
+      <h3>人口推移</h3>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart data={data} margin={{ top: 32, right: 32, left: 24 }}>
+          <XAxis dataKey="year" fontSize={12}>
+            <Label value="年度" position="right" offset={16} />
+          </XAxis>
+          <YAxis fontSize={12}>
+            <Label value="人口数" position="top" offset={16} />
+          </YAxis>
+          <Legend
+            onMouseEnter={handleLegendMouseEnter}
+            onMouseOut={handleLegendMouseOut}
+            align="right"
+            layout="vertical"
+            verticalAlign="top"
+            wrapperStyle={{ paddingLeft: '4rem', overflowY: 'auto' }}
           />
-        ))}
-      </LineChart>
-    </ResponsiveContainer>
+          <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+          <Tooltip />
+          {prefectures.map((prefecture) => (
+            <Line
+              strokeWidth={targetDatakey === `${prefecture.prefCode}` ? 4 : 1}
+              key={`line-${prefecture.prefCode}`}
+              name={`${prefecture.prefName}`}
+              type="monotone"
+              dataKey={`${prefecture.prefCode}`}
+              stroke={`hsl(${(prefecture.prefCode * 360) / 47}, 75%, 60%)`}
+            />
+          ))}
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
+
+export default memo(PopulationGraph)
