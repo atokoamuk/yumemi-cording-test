a8d82d190ea4404e28e0c0c220b9fdcb4a788508
atokoamuk <kt.kmo814@outlook.com>
2025-04-09 11:37:55 +0900

    feat: SimpleButtonコンポーネントのカスタマイズを追加
    - PropsにclassNameを追加してスタイルのカスタマイズに対応
    - buttonの拡張コンポーネントとして型定義

--- src/components/SimpleButton.tsx
+++ src/components/SimpleButton.tsx
@@ -1,15 +1,27 @@
 import React, { memo } from 'react'
+import { ButtonHTMLAttributes } from 'react'
 
-interface Props {
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
   onClick: () => void
   children: React.ReactNode
+  className?: string
 }
 
 function SimpleButton(props: Props) {
-  const { onClick, children } = props
+  const { onClick, children, ...buttonProps } = props
   return (
     <button
-      className="bg-white hover:bg-gray-100 text-gray-800  py-1 px-2 border border-gray-400 rounded shadow"
+      {...buttonProps}
+      className={`bg-white
+         hover:bg-gray-100
+          text-gray-800 
+          py-1 
+          px-2 
+          border 
+          border-gray-400 
+          rounded 
+          shadow
+          ${props.className}`}
       onClick={onClick}
     >
       {children}
