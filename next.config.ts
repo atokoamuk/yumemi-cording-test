adf199e747f3436e3d439bca74ebc8b8118bb221
atokoamuk <kt.kmo814@outlook.com>
2025-04-10 16:21:40 +0900

    chore: next.config.tsの設定を簡素化
    - 開発環境のdistDir設定を削除

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from 'next'
 
-const isDev = process.env.NODE_ENV === 'development'
-
-const nextConfig: NextConfig = {
-  distDir: isDev ? undefined : 'dist',
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
